5af88b57a50b485439bb7a41adced99e82870a1d
Yavor Spassov <ygspasov@gmail.com>
2022-08-30 20:40:13 +0300

    Adding UI elements.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,63 @@
 <template>
-  <hello-world />
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Crypto Trader</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer fixed temporary v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Crypto Trader </v-list-item-title>
+          <v-list-item-subtitle>
+            Create an account and trade crypto
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <the-lists></the-lists>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
+import TheLists from '../components/TheLists.vue';
 export default {
-  name: 'HomeView',
-
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+      { title: 'Photos', icon: 'mdi-image' },
+      { title: 'About', icon: 'mdi-help-box' },
+    ],
+    right: null,
+  }),
   components: {
-    HelloWorld,
+    TheLists,
   },
 };
 </script>

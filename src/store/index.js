1ccadbaeba2c009701e7fd14a76e193ea2b7ae32
Yavor Spassov <ygspasov@gmail.com>
2022-09-01 13:56:23 +0300

    Fetching crypto data from Nomics.com and adding an environment variable to hide the API key.

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,123 @@
 import { defineStore } from 'pinia';
-
+const APIkey = process.env.VUE_APP_MY_ENV_VARIABLE;
 export const useCriptoStore = defineStore('cryptoStore', {
-  state: () => ({ crypto: ['BTC', 'ETH'] }),
+  state: () => ({
+    crypto: [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        sodium: 87,
+        calcium: '14%',
+        iron: '1%',
+      },
+      {
+        name: 'Ice cream sandwich',
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        sodium: 129,
+        calcium: '8%',
+        iron: '1%',
+      },
+      {
+        name: 'Eclair',
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        sodium: 337,
+        calcium: '6%',
+        iron: '7%',
+      },
+      {
+        name: 'Cupcake',
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        sodium: 413,
+        calcium: '3%',
+        iron: '8%',
+      },
+      {
+        name: 'Gingerbread',
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        sodium: 327,
+        calcium: '7%',
+        iron: '16%',
+      },
+      {
+        name: 'Jelly bean',
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        sodium: 50,
+        calcium: '0%',
+        iron: '0%',
+      },
+      {
+        name: 'Lollipop',
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        sodium: 38,
+        calcium: '0%',
+        iron: '2%',
+      },
+      {
+        name: 'Honeycomb',
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        sodium: 562,
+        calcium: '0%',
+        iron: '45%',
+      },
+      {
+        name: 'Donut',
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        sodium: 326,
+        calcium: '2%',
+        iron: '22%',
+      },
+      {
+        name: 'KitKat',
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        sodium: 54,
+        calcium: '12%',
+        iron: '6%',
+      },
+    ],
+  }),
   getters: {},
-  actions: {},
+  actions: {
+    async loadCrypto() {
+      const response = await fetch(
+        `https://api.nomics.com/v1/currencies/ticker?key=${APIkey}`
+      );
+      console.log(response);
+      const responseData = await response.json();
+      console.log(responseData);
+      if (!response.ok) {
+        const error = new Error(responseData.message || 'Failed to fetch');
+        throw error;
+      }
+    },
+  },
 });
